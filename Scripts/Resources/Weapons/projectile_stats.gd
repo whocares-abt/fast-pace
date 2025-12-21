c2a1ba6b9167c477860c3c6864c8b10b7cd9a29b
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-21 19:00:22 +0530

    Added resources for projectiles and weapons

---
+++ Scripts/Resources/Weapons/projectile_stats.gd
@@ -0,0 +1,6 @@
+class_name ProjectileStats
+extends Resource
+
+@export var projectile_name : String
+@export var texture : Texture2D
+var hitbox : Area2D
---
+++ Scripts/Resources/Weapons/weapon_stats.gd
@@ -0,0 +1,20 @@
+class_name WeaponStats
+extends Resource
+
+@export var ranged : bool
+@export var projectile : ProjectileStats
+@export var texture : Texture2D
+
+# Stats for both ranged and melee weapons
+@export var firing_rate : float
+@export var damage : float
+
+# Stats for melee weapons
+var attack_hitbox : Area2D
+
+# Stats for ranged weapons
+@export var spread : float
+
+# Number of bullets shot in one shot and the interval range
+@export var pellet_count : int
+@export var pellet_rate : float # Time between each pellet
