52b125d5acff7ac767775304bcf3a4afa057eaa6
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-21 18:13:35 +0530

    Added basic player character and debug map

---
+++ Scripts/Characters/player_character.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
+
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
