0eec3e18c524263f102cef7dc115df4fe5eb93a7
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-23 13:16:24 +0530

    Basic navigation working

---
+++ Scripts/AI/navigation_component.gd
@@ -0,0 +1,21 @@
+extends NavigationAgent2D
+
+var goal : Node2D
+@export var time = 1
+@export var path_recalc_threshold = 10
+
+@onready var nav_timer = $NavTimer
+
+func set_goal(new_goal):
+	goal = new_goal
+
+func _on_nav_timer_timeout() -> void:
+	if (goal == null):
+		return
+	
+	if (target_position - goal.global_position).length() > path_recalc_threshold:
+		target_position = goal.global_position
+	nav_timer.start(time)
+
+	if (not is_target_reachable()):
+		print("ASDSA")
--- Scripts/Characters/enemy_character.gd
+++ Scripts/Characters/enemy_character.gd
@@ -1,15 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
-const SPEED = 300
+@export var SPEED = 300
 
 @onready var particle = $GPUParticles2D
-
 @onready var animation_sprite = $AnimatedSprite2D
+
 @onready var collision_body = $CollisionShape2D
 @onready var hitbox = $Hitbox
 
-func _physics_process(delta: float) -> void:
-	pass
+@onready var nav_comp = $NavComp
+
+@export var goal : Node2D
+
+func _ready() -> void:
+	update_goal(goal)
+
+func update_goal(new_goal):
+	nav_comp.set_goal(new_goal)
+
+func _physics_process(_delta: float) -> void:	
+	if (not nav_comp.is_target_reached()):
+		var nav_point_direction = to_local(nav_comp.get_next_path_position()).normalized()
+		velocity = nav_point_direction*SPEED
+		move_and_slide()
+
+# Func for enemy death
 
 func _on_hitbox_hurtbox_entered(area: Variant) -> void:
 	if ("Player" in area.hurtbox_owners):
@@ -25,7 +40,6 @@ func disable_enemy():
 	# To prevent blood splashes from disappearing
 	await get_tree().create_timer(particle.lifetime).timeout
 	pause_particle_process()
-
 
 func pause_particle_process():
 	# To prevent particles from despawning - blood splatters stay
