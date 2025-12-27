8280d904022087656f8550387f387ee2e62c001c
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-27 22:17:36 +0530

    Fixed navigation paths and increased player hitbox

--- Scripts/AI/navigation_component.gd
+++ Scripts/AI/navigation_component.gd
@@ -6,6 +6,9 @@ var goal : Node2D
 
 @onready var nav_timer = $NavTimer
 
+func _ready() -> void:
+	debug_enabled = false
+
 func set_goal(new_goal):
 	goal = new_goal
 	if (goal != null):
@@ -16,6 +19,6 @@ func _on_nav_timer_timeout() -> void:
 	if (goal == null):
 		nav_timer.stop()
 		return
-	
+
 	#if (target_position - goal.global_position).length() > path_recalc_threshold:
 	target_position = goal.global_position
